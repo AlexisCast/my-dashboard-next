75ec685a992867f6f3555b1766f2dd61da866477
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-13 01:47:16 -0600

    display pokemon images

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,19 +1,39 @@
-const getPokemons = async (limit = 20, offset = 0) => {
-	const response = await fetch(
-		`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-	);
-	const data = response.json();
+import Image from "next/image";
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
 
-	return data;
+const getPokemons = async (
+	limit = 20,
+	offset = 0
+): Promise<SimplePokemon[]> => {
+	const data: PokemonsResponse = await fetch(
+		`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+	).then((res) => res.json());
+
+	const pokemons = data.results.map((pokemon) => ({
+		id: String(pokemon.url.split("/").at(-2)),
+		name: pokemon.name,
+	}));
+
+	return pokemons;
 };
 
 export default async function PokemonsPage() {
-	const pokemons = await getPokemons();
+	const pokemons = await getPokemons(151);
 
 	return (
-		<div>
-			<h1>Pokemons Page</h1>
-			{JSON.stringify(pokemons)}
+		<div className="flex flex-col">
+			<div className="flex flex-wrap gap-10 items-center justify-center">
+				{pokemons.map((pokemon) => (
+					<div key={pokemon.id}>
+						<Image
+							src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+							alt="Pokemon Avatar"
+							width={100}
+							height={100}
+						/>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
