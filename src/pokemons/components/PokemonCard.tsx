f7f729aee9b4d0e7fc1bc8e0f415d6330c0b6ce3
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-17 02:58:56 -0600

    implement new route to display a pokemon by ID

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -30,7 +30,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
 					<div className="mt-5">
 						<Link
 							className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
-							href={`dashboard/pokemon/${id}`}
+							href={`/dashboard/pokemon/${id}`}
 						>
 							More Information
 						</Link>
